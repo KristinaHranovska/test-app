f060b2ca63835715789d3457ba1f9d37723eb6df
Kristina <kr.hranovska@gmail.com>
2024-02-29 18:05:01 +0200

    lesson 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -103,9 +103,40 @@
 
 // export default App;
 
-import FeedbackForm from "./Product";
+import { useState } from "react";
+import ArticleList from "./ArticleList";
+import { fetchArticlesWithTopic } from "../articles-api";
+import SearchForm from "./SearchForm";
+
 const App = () => {
-  return <FeedbackForm />;
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleSearch = async (topic) => {
+    try {
+      setArticles([]); // для очищення попереднього стану articles, перед новим запитом
+      setLoading(true);
+      setError(false); // щоб скинути попередню помилку перед наступним запитом (якщо вона була)
+      const data = await fetchArticlesWithTopic(topic);
+      setArticles(data);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <SearchForm onSearch={handleSearch} />
+      {loading && <p>Loading data, please wait...</p>}
+      {error && (
+        <p>Whoops, something went wrong! Please try reloading this page!</p>
+      )}
+      {articles.length > 0 && <ArticleList items={articles} />}
+    </div>
+  );
 };
 
 export default App;
